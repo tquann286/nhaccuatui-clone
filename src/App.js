8c38db277382d3dc39cf47d2799d954779e2b903
Quan Trung <quantrung286@gmail.com>
2022-06-11 14:54:03 +0700

    up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 import './App.scss'
 
+import {
+  getHome,
+  getSong,
+  getPlaylists,
+  //... and many other services
+} from "nhaccuatui-api-full";
+
 const App = () => {
+  getSong("SoaMoVlAX7lW").then((data) => console.log(data))
   return (
     <div>
       <h1>App</h1>
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Homepage.scss'
+
+const Homepage = () => {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
