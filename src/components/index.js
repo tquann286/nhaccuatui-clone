55a20c8d4a5795f8f4e151a21e47882e26f8c997
Quan Trung <quantrung286@gmail.com>
2022-06-11 15:38:26 +0700

    import hp-scss

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as MainHomepage } from './MainHomepage/MainHomepage'
+export { default as LeftSidebar } from './LeftSidebar/LeftSidebar'
+export { default as RightSidebar } from './RightSidebar/RightSidebar'
