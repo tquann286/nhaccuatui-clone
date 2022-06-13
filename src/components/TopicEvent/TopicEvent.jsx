1e0f3bb75825bc0f97a1cfa026482bfcfac467fd
Quan Trung <quantrung286@gmail.com>
2022-06-13 21:59:52 +0700

    swiper intergrate

--- src/components/TopicEvent/TopicEvent.jsx
+++ src/components/TopicEvent/TopicEvent.jsx
@@ -3,17 +3,33 @@ import './TopicEvent.scss'
 
 import { getTopicEventTitle } from 'services/TopicEvent'
 
-const TopicEvent = ({ topicEvent = [] }) => {
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
+const TopicEvent = ({ topicEvent = [] }) => {
 	return (
 		<div className='te-container'>
 			{topicEvent.map((topic) => {
 				const { vieTitle } = getTopicEventTitle(topic.groupName)
 				return (
 					<div key={vieTitle} className='tp-container'>
-						<div className='tp-title'>{vieTitle}</div>
+						<div className='tp-header'>
+							<div className="tp-title">{vieTitle}</div>
+							<div className="tp-nav"></div>
+						</div>
 						<div className='tp-main'>
-							
+							<Swiper slidesPerView={4} spaceBetween={8}>
+								{topic.listPlaylist.map((playlist) => {
+									return (
+										<SwiperSlide key={playlist.key}>
+											<div className='pl-container'>
+												<img src={playlist.thumbnail} alt={playlist.title} />
+												<div className='pl-title'>{playlist.title}</div>
+											</div>
+										</SwiperSlide>
+									)
+								})}
+							</Swiper>
 						</div>
 					</div>
 				)
