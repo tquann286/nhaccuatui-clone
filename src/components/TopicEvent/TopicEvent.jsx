45276a3b949814135da08a4da17a6840d4184449
Quan Trung <quantrung286@gmail.com>
2022-06-13 20:16:11 +0700

    change from slick to swiper

--- src/components/TopicEvent/TopicEvent.jsx
+++ src/components/TopicEvent/TopicEvent.jsx
@@ -2,24 +2,8 @@ import React from 'react'
 import './TopicEvent.scss'
 
 import { getTopicEventTitle } from 'services/TopicEvent'
-import { CustomNextArrow, CustomPrevArrow } from './CustomArrow/CustomArrow'
-
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import Slider from 'react-slick'
 
 const TopicEvent = ({ topicEvent = [] }) => {
-	console.log(topicEvent)
-
-	const settings = {
-    accessibility: false,
-		infinite: false,
-		speed: 800,
-		slidesToShow: 4,
-		slidesToScroll: 3,
-    nextArrow: <CustomNextArrow />,
-    prevArrow: <CustomPrevArrow />
-	}
 
 	return (
 		<div className='te-container'>
@@ -29,18 +13,7 @@ const TopicEvent = ({ topicEvent = [] }) => {
 					<div key={vieTitle} className='tp-container'>
 						<div className='tp-title'>{vieTitle}</div>
 						<div className='tp-main'>
-							<Slider {...settings}>
-								{topic.listPlaylist.map((playlist) => {
-									return (
-										<div className='pl-container'>
-											<div className='pl-img'>
-												<img src={playlist.thumbnail} alt={playlist.title} />
-											</div>
-											<div className='pl-title'>{playlist.title}</div>
-										</div>
-									)
-								})}
-							</Slider>
+							
 						</div>
 					</div>
 				)
