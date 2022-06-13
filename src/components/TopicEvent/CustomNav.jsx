a055f88b6924eb18bb585c5392f29b896308224f
Quan Trung <quantrung286@gmail.com>
2022-06-13 22:43:22 +0700

    custom nav

---
+++ src/components/TopicEvent/CustomNav.jsx
@@ -0,0 +1,19 @@
+// some-inner-component.jsx
+import { React } from 'react'
+import { useSwiper } from 'swiper/react'
+
+export const SlideNextButton = () => {
+	const swiper = useSwiper()
+
+	return (
+		<button onClick={() => swiper.slideNext()}>Slide to the next slide</button>
+	)
+}
+
+export const SlidePrevButton = () => {
+	const swiper = useSwiper()
+
+	return (
+		<button onClick={() => swiper.slidePrev()}>Slide to the Prev slide</button>
+	)
+}
--- src/components/TopicEvent/TopicEvent.jsx
+++ src/components/TopicEvent/TopicEvent.jsx
@@ -5,9 +5,9 @@ import { getTopicEventTitle } from 'services/TopicEvent'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
+import { SlidePrevButton, SlideNextButton } from './CustomNav'
 
 const TopicEvent = ({ topicEvent = [] }) => {
-
 	return (
 		<div className='te-container'>
 			{topicEvent.map((topic) => {
@@ -15,23 +15,25 @@ const TopicEvent = ({ topicEvent = [] }) => {
 				return (
 					<div key={vieTitle} className='tp-container'>
 						<div className='tp-header'>
-							<div className="tp-title">{vieTitle}</div>
-							<div className="tp-nav"></div>
+							<div className='tp-title'>{vieTitle}</div>
+							<div className='tp-nav'></div>
 						</div>
 						<div className='tp-main'>
-						<Swiper slidesPerView={4} spaceBetween={8}>
+							<Swiper slidesPerView={4} spaceBetween={8}>
+							<SlideNextButton />
 								{topic.listPlaylist.map((playlist) => {
+									const { key, thumbnail, title } = playlist
 
 									return (
-										<SwiperSlide key={playlist.key}>
+										<SwiperSlide key={key}>
 											<div className='pl-container'>
-												<img src={playlist.thumbnail} alt={playlist.title} />
-												<div className='pl-title'>{playlist.title}</div>
+												<img src={thumbnail} alt={title} />
+												<div className='pl-title' title={title}>{title}</div>
 											</div>
 										</SwiperSlide>
 									)
 								})}
-								</Swiper>
+							</Swiper>
 						</div>
 					</div>
 				)
