74073281aeefc4a77f842e8e63b9db405b902c80
Quan Trung <quantrung286@gmail.com>
2022-06-15 21:25:13 +0700

    done design layout

--- src/components/NewRelease/NewRelease.jsx
+++ src/components/NewRelease/NewRelease.jsx
@@ -20,7 +20,7 @@ const NewRelease = ({ newRelease: { song: newSong } }) => {
 						thumbs={{ swiper: thumbsSwiper }}
 						autoplay={{
 							delay: 4000,
-							disableOnInteraction: false,
+							disableOnInteraction: true,
 						}}
 						centeredSlides={true}
 						loop={true}
@@ -32,9 +32,37 @@ const NewRelease = ({ newRelease: { song: newSong } }) => {
 
 							return (
 								<SwiperSlide key={key}>
-									<div className='nr-active-img' title={title}>
-										<img src={thumbnail} alt={title} />
-									</div>
+									{({isActive}) => isActive && (
+										<div className='nr-active-container'>
+											<div className='nr-active-img' title={title}>
+												<img src={thumbnail} alt={title} />
+											</div>
+											<div className='nr-active-detail'>
+												<h4>{title}</h4>
+												<div className='nr-artist-container'>
+													<div className='nr-artist-main'>
+														{artists.map((artist) => {
+															const { artistId, imageUrl, name, shortLink } =
+																artist
+	
+															return (
+																<div key={artistId} className='nr-artist-img'>
+																	<img src={imageUrl} alt={name} />
+																</div>
+															)
+														})}
+														<div className='nr-artist-name'>
+															{artists.map((artist) => {
+																const { artistId, name, shortLink } = artist
+	
+																return <span key={artistId}>{name}</span>
+															})}
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									)}
 								</SwiperSlide>
 							)
 						})}
