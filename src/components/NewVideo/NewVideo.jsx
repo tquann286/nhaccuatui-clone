19cc212e586adf234ad667ce68b9f0c10dd35d3e
Quan Trung <quantrung286@gmail.com>
2022-06-20 11:32:41 +0700

    add dark color mixin

--- src/components/NewVideo/NewVideo.jsx
+++ src/components/NewVideo/NewVideo.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import './NewVideo.scss'
 
-const NewVideo = () => {
+import { cloneDeep } from 'lodash'
+
+const NewVideo = ({ videos }) => {
+  const [newVideo, setNewVideo] = useState([])
+  console.log(newVideo)
+
+  useEffect(() => {
+    const cloneVideos = cloneDeep(videos)
+
+    setNewVideo(cloneVideos.slice(0,6))
+  }, [])
+
   return (
-    <div>NewVideo</div>
+    <div className="nv-container">
+      <div className="nv-title">
+        <Link to='/'>Video hot</Link>
+      </div>
+    </div>
   )
 }
 
