113ebddd16edc97015644dbab33736240e659771
Quan Trung <quantrung286@gmail.com>
2022-06-20 22:10:17 +0700

    uninstall chart js

--- src/components/NewVideo/VideoDetail.jsx
+++ src/components/NewVideo/VideoDetail.jsx
@@ -1,24 +1,76 @@
-import { BsPlayCircleFill } from 'react-icons/bs'
+import React, { useState, useEffect, useRef } from 'react'
 
-const VideoDetail = ({ keyId, artists, duration, thumbnail, title, height }) => {
+import { BsLink45Deg, BsPlayCircleFill } from 'react-icons/bs'
+import { IoMdMore } from 'react-icons/io'
+import { SiApplemusic } from 'react-icons/si'
 
+import { isEmpty } from 'lodash'
 
-  return (
-    <div className='vd-container'>
-      <div className="vd-video" title={title} style={{ height }}>
-        <img className='vd-img' src={thumbnail} alt={title} />
-        <div className="vd-duration">{duration}</div>
-        <div className="vd-blur-layer">
-          <div className="vd-play-icon">
-            <BsPlayCircleFill />
-          </div>
-          <div className="vd-more-options">
-          
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+const VideoDetail = ({
+	keyId,
+	artists,
+	duration,
+	thumbnail,
+	title,
+	height,
+	refMapping,
+}) => {
+	const [showMoreOptions, setShowMoreOptions] = useState(false)
+	const [showMorePosition, setShowMorePosition] = useState({ transform: `translate(${0}px, ${0}px)` })
+	const videoRef = useRef(null)
+
+  useEffect(() => {
+    if (videoRef) {
+      console.log(videoRef.current.getBoundingClientRect())
+      setShowMorePosition()
+    }
+  })
+
+	const toggleShowMore = () => {
+		setShowMoreOptions(!showMoreOptions)
+	}
+
+	const handleMoreOptions = (e) => {
+		e.stopPropagation()
+		toggleShowMore()
+	}
+
+	return (
+		<React.Fragment>
+			<div className='vd-container' ref={videoRef}>
+				<div className='vd-video' title={title} style={{ height }}>
+					<img className='vd-img' src={thumbnail} alt={title} />
+					<div className='vd-duration'>{duration}</div>
+					<div className='vd-blur-layer'>
+						<div className='vd-play-icon'>
+							<BsPlayCircleFill />
+						</div>
+						<div
+							title='Thêm'
+							className='vd-more-options'
+							onClick={(e) => handleMoreOptions(e)}
+						>
+							<IoMdMore />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='vd-more-options-box' style={showMorePosition}>
+				<ul>
+					{!isEmpty(refMapping) && (
+						<li>
+							<SiApplemusic />
+							<span>Nghe audio</span>
+						</li>
+					)}
+					<li>
+						<BsLink45Deg />
+						<span>Sao chép link</span>
+					</li>
+				</ul>
+			</div>
+		</React.Fragment>
+	)
 }
 
 export default VideoDetail
