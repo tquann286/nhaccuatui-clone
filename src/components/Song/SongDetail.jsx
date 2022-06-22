f7cc1f46dfc7df459ce0d8c682691620f9458879
Quan Trung <quantrung286@gmail.com>
2022-06-22 22:44:03 +0700

    more options song details

--- src/components/Song/SongDetail.jsx
+++ src/components/Song/SongDetail.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
+import { BsHeadphones } from 'react-icons/bs'
+import { IoMdMore } from 'react-icons/io'
+
 import { createSongUrl, createArtistUrl } from 'share/utilities'
+import { createRandomSongView } from 'services/SongDetail'
 
 const SongDetail = ({ artists, songId, thumbnail, title }) => {
-
+  const songView = createRandomSongView()
   
+
 	return (
 		<div className='sd-container'>
 			<div className='sd-main'>
         <Link to={createSongUrl(title, songId)} className="sd-thumbnail" title={title}>
           <div className='sd-thumb-img' style={{ backgroundImage: `url(${thumbnail})` }}></div>
         </Link>
+        <div className="sd-more">
+          <div className="sd-view-count">
+            <BsHeadphones />
+            <div className="sc-view-number">{songView}</div>
+          </div>
+          <div className="sd-more-options">
+            <div className="sd-three-dots">
+              <IoMdMore />
+            </div>
+          </div>
+        </div>
         <div className="sd-song-details">
           <div className="sd-song-title">
             <Link to={createSongUrl(title, songId)}>{title}</Link>
---
+++ src/services/SongDetail.js
@@ -0,0 +1,11 @@
+import millify from 'millify'
+
+export const createRandomSongView = () => {
+  const songView = Math.floor(Math.random() * 100000)
+  if (songView > 1000 && songView < 1000000) {
+    console.log(millify(songView))
+    return millify(songView)
+  } else {
+    createRandomSongView()
+  }
+}
