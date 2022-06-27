feded081e8d8489510339c7b6b3a55fc52db91c1
Quan Trung <quantrung286@gmail.com>
2022-06-27 13:58:23 +0700

    add nct logo

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
 import './LeftSidebar.scss'
+import { Link } from 'react-router-dom';
+import nctLogo from 'images/nct-logo.png'
 
 const LeftSidebar = () => {
 	return (
-		<div className='left-sidebar'>left-sidebar</div>
+		<div className='left-sidebar'>
+			<div className="ls-logo-theme">
+				<Link className='ls-logo' to='/'><img src={nctLogo} alt="NCT" /></Link>
+			</div>
+		</div>
 	)
 }
 
