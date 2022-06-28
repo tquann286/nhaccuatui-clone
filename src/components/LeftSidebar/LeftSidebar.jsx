b3a27fc3955a1bc4d477967b5c139d0858f53268
Quan Trung <quantrung286@gmail.com>
2022-06-28 10:16:36 +0700

    toggle change theme

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,53 +1,49 @@
 import React from 'react'
 import './LeftSidebar.scss'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import nctLogo from 'images/nct-logo.png'
 
-import {useStore, actions} from 'store'
-import {BsMoonStarsFill, BsSunFill} from 'react-icons/bs'
+import { useStore, actions } from 'store'
+import { BsMoonStarsFill, BsSunFill } from 'react-icons/bs'
 
 const LeftSidebar = () => {
-	const [state, dispatch] = useStore()
-	const {theme} = state
-	console.log(theme)
+  const [state, dispatch] = useStore()
+  const { theme } = state
+  console.log(theme)
 
-	const handleChangeTheme = () => {
-		if (theme === 'light') {
-			dispatch(actions.changeDarkTheme())
-			localStorage.setItem('theme', 'dark')
-		} else if (theme === 'dark') {
-			dispatch(actions.changeLightTheme())
-			localStorage.setItem('theme', 'light')
-		}
-	}
+  const handleChangeTheme = () => {
+    if (theme === 'light') {
+      dispatch(actions.changeDarkTheme())
+      localStorage.setItem('theme', 'dark')
+    } else if (theme === 'dark') {
+      dispatch(actions.changeLightTheme())
+      localStorage.setItem('theme', 'light')
+    }
+  }
 
-	return (
-		<div className='left-sidebar'>
-			<div className='ls-logo-theme'>
-				<Link className='ls-logo' to='/'>
-					<img src={nctLogo} alt='NCT' />
-				</Link>
-				<div className='theme-container'>
-					<input type='checkbox' name='theme' id='theme' />
-					<label htmlFor='check'>
-						<div className='box'>
-							<div className='ball'>
-								<div className='scenary'>
-									<div className='moon'>
-										<BsMoonStarsFill />
-									</div>
-									<div className='sun'>
-										<BsSunFill />
-									</div>
-								</div>
-							</div>
-						</div>
-					</label>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className='left-sidebar'>
+      <div className='ls-logo-theme'>
+        <Link className='ls-logo' to='/'>
+          <img src={nctLogo} alt='NCT' />
+        </Link>
+        <div className='theme-container'>
+          <div className='box' onClick={handleChangeTheme}>
+            <div className='ball' style={theme === 'dark' ? { transform: 'translateX(100%)' } : {}}></div>
+            <div className='scenary'>
+              <div className='moon'>
+                <BsMoonStarsFill />
+              </div>
+              <div className='sun'>
+                <BsSunFill />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default LeftSidebar
