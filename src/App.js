2b69fb27a97b3d7672c9243989610fddd86e1d37
Quan Trung <quantrung286@gmail.com>
2022-06-29 20:54:57 +0700

    try to add dark mode

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React, {useEffect} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { useStore, actions } from 'store';
+import { useStore, actions } from 'store'
 
 import './App.scss'
-import {Homepage, NotFound} from 'pages'
+import { Homepage, NotFound } from 'pages'
 
 const App = () => {
-	const [state, dispatch] = useStore()
+  const [state, dispatch] = useStore()
 
-	useEffect(() => {
-		const localTheme = localStorage.getItem('theme')
-		if (localTheme) {
-			dispatch(actions.setTheme(localTheme))
-		} else {
-			localStorage.setItem('theme', 'light')
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme')
+    if (localTheme) {
+      dispatch(actions.setTheme(localTheme))
+    } else {
+      localStorage.setItem('theme', 'light')
+    }
+
+    console.log(localTheme)
+  }, [])
+
+  useEffect(() => {
+		if (state.theme) {
+			document.body.setAttribute('data-theme', state.theme)
 		}
+	}, [state.theme])
 
-		console.log(localTheme)
-	}, [])
-
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<Homepage />} />
-				<Route path='*' element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
