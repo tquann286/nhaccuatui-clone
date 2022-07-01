207a5d6b1fb88b20d5a3ad754ccf1548e948acf4
Quan Trung <quantrung286@gmail.com>
2022-07-01 22:13:18 +0700

    done add language test

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,14 +1,27 @@
-import { SET_THEME, CHANGE_LIGHT_THEME, CHANGE_DARK_THEME} from 'share/constants'
+import { SET_THEME, CHANGE_LIGHT_THEME, CHANGE_DARK_THEME, CHANGE_VI_LANG, CHANGE_EN_LANG, SET_LANG } from 'share/constants'
 
 export const setTheme = (payload) => ({
-	type: SET_THEME,
-	payload,
+  type: SET_THEME,
+  payload,
 })
 
 export const changeLightTheme = () => ({
-	type: CHANGE_LIGHT_THEME,
+  type: CHANGE_LIGHT_THEME,
 })
 
 export const changeDarkTheme = () => ({
-	type: CHANGE_DARK_THEME,
+  type: CHANGE_DARK_THEME,
+})
+
+export const setLang = (payload) => ({
+  type: SET_LANG,
+  payload,
+})
+
+export const changeViLang = () => ({
+  type: CHANGE_VI_LANG,
+})
+
+export const changeEnLang = () => ({
+  type: CHANGE_EN_LANG,
 })
