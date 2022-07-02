74c466db225e42bec211dac18700ccbae7c12fd7
Quan Trung <quantrung286@gmail.com>
2022-07-02 15:25:30 +0700

    refactor code with prettier

--- src/services/VideoDetail.js
+++ src/services/VideoDetail.js
@@ -1,21 +1,17 @@
 import { replaceDashUrl } from 'share/utilities'
-import removeVietnameseTones from 'share/removeVietnameseTones'
+import { removeVietnameseTones } from 'share'
 
 export const handleVideoLink = (keyId, title, artists) => {
-	const artistLink = artists.reduce((acc, cur, i) => {
-		if (!cur.shortLink) {
-			return `${acc}${i > 0 ? '-ft-' : ''}${replaceDashUrl(removeVietnameseTones(cur.name))}`
-		} 
-		if (acc) {
-			return `${acc}${i > 0 ? '-ft-' : ''}${cur.shortLink}`
-		}
-		return `${cur.shortLink}`
-	}, '')
-	const videoLink =
-		`video/${replaceDashUrl(
-			removeVietnameseTones(title)
-		)}-${artistLink}`.toLocaleLowerCase() + `&k=${keyId}`
+  const artistLink = artists.reduce((acc, cur, i) => {
+    if (!cur.shortLink) {
+      return `${acc}${i > 0 ? '-ft-' : ''}${replaceDashUrl(removeVietnameseTones(cur.name))}`
+    }
+    if (acc) {
+      return `${acc}${i > 0 ? '-ft-' : ''}${cur.shortLink}`
+    }
+    return `${cur.shortLink}`
+  }, '')
+  const videoLink = `video/${replaceDashUrl(removeVietnameseTones(title))}-${artistLink}`.toLocaleLowerCase() + `&k=${keyId}`
 
-
-	return videoLink
+  return videoLink
 }
--- src/share/utilities.js
+++ src/share/utilities.js
@@ -1,91 +1,91 @@
-import removeVietnameseTones from 'share/removeVietnameseTones'
+import { removeVietnameseTones } from 'share'
 import { toast } from 'react-toastify'
 
 export const getNavigateUrl = (url) => {
-	const startIndex = url.indexOf('nhaccuatui.com/') + 15
+  const startIndex = url.indexOf('nhaccuatui.com/') + 15
 
-	if (startIndex !== 14) {
-		return url.substring(startIndex)
-	} else {
-		return '/'
-	}
+  if (startIndex !== 14) {
+    return url.substring(startIndex)
+  } else {
+    return '/'
+  }
 }
 
 export const getPlaylistKeyId = (url) => {
-	const startIndex = url.indexOf('va.') + 3
-	const keyIdLength = 12
+  const startIndex = url.indexOf('va.') + 3
+  const keyIdLength = 12
 
-	if (startIndex) {
-		return url.substring(startIndex, keyIdLength)
-	} else {
-		return null
-	}
+  if (startIndex) {
+    return url.substring(startIndex, keyIdLength)
+  } else {
+    return null
+  }
 }
 
 export const replaceDashUrl = (url) => {
-	return url.replaceAll(' ', '-')
+  return url.replaceAll(' ', '-')
 }
 
 export const createPlaylistUrl = (title, keyId) => {
-	const playlistTitle = replaceDashUrl(title)
+  const playlistTitle = replaceDashUrl(title)
 
-	return `/playlist/${playlistTitle}.va.${keyId}`
+  return `/playlist/${playlistTitle}.va.${keyId}`
 }
 
 export const covertTimestamp = (time) => {
-	const date = new Date(time)
+  const date = new Date(time)
 
-	return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 }
 
 export const createSongUrl = (title, keyId) => {
-	if (title && keyId) {
-		return `bai-hat/${replaceDashUrl(removeVietnameseTones(title))}&k=${keyId}`
-	} else {
-		return '/'
-	}
+  if (title && keyId) {
+    return `bai-hat/${replaceDashUrl(removeVietnameseTones(title))}&k=${keyId}`
+  } else {
+    return '/'
+  }
 }
 
 export const createArtistUrl = (name, shortLink, artistId) => {
-	if (name && shortLink && artistId) {
-		return `nghe-si/${shortLink}&k=${artistId}`
-	} else {
-		return `tim-kiem/q=${replaceDashUrl(name)}`
-	}
+  if (name && shortLink && artistId) {
+    return `nghe-si/${shortLink}&k=${artistId}`
+  } else {
+    return `tim-kiem/q=${replaceDashUrl(name)}`
+  }
 }
 
 export const createTopicUrl = (title, keyId) => {
-	if (title && keyId) {
-		return `chu-de/${replaceDashUrl(removeVietnameseTones(title))}&k=${keyId}`
-	} else {
-		return '/'
-	}
+  if (title && keyId) {
+    return `chu-de/${replaceDashUrl(removeVietnameseTones(title))}&k=${keyId}`
+  } else {
+    return '/'
+  }
 }
 
 export const createTop100Url = (title, keyId) => {
-	if (title && keyId) {
-		return `top-100/${replaceDashUrl(removeVietnameseTones(title))}&k=${keyId}`
-	} else {
-		return '/'
-	}
+  if (title && keyId) {
+    return `top-100/${replaceDashUrl(removeVietnameseTones(title))}&k=${keyId}`
+  } else {
+    return '/'
+  }
 }
 
 export const copyNotify = () =>
-	toast('🦄 Đã sao chép link.', {
-		position: 'bottom-left',
-		autoClose: 3000,
-	})
+  toast('🦄 Đã sao chép link.', {
+    position: 'bottom-left',
+    autoClose: 3000,
+  })
 
 export const toastConfig = {
-	position: 'bottom-left',
-	autoClose: 3000,
-	newestOnTop: true,
+  position: 'bottom-left',
+  autoClose: 3000,
+  newestOnTop: true,
 }
 
 export const animationConfig = {
-	animationIn: 'fadeIn',
-	animationOut: 'fadeOut',
-	animationInDuration: 100,
-	animationOutDuration: 100,
-	style: { zIndex: 1 },
+  animationIn: 'fadeIn',
+  animationOut: 'fadeOut',
+  animationInDuration: 100,
+  animationOutDuration: 100,
+  style: { zIndex: 1 },
 }
