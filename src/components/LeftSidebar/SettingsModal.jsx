3b48ec0f637314db35d0ada02b25ff7d721dfa80
Quan Trung <quantrung286@gmail.com>
2022-07-04 14:42:14 +0700

    done scss layout settings modal

--- src/components/LeftSidebar/SettingsModal.jsx
+++ src/components/LeftSidebar/SettingsModal.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { MdLanguage, MdWbSunny } from 'react-icons/md'
 import { BiSupport } from 'react-icons/bi'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { MdArrowForwardIos } from 'react-icons/md'
 
 const SettingsModal = ({ theme, dispatch }) => {
   return (
@@ -10,6 +11,7 @@ const SettingsModal = ({ theme, dispatch }) => {
       <div className='sm-item sm-languages'>
         <MdLanguage className='sm-icon' />
         <span className='sm-title'>Ngôn ngữ</span>
+        <MdArrowForwardIos className='ar-icon'/>
       </div>
       <div className='sm-item sm-support'>
         <BiSupport className='sm-icon' />
@@ -18,6 +20,7 @@ const SettingsModal = ({ theme, dispatch }) => {
       <div className='sm-item sm-themes'>
         {theme === 'light' ? <MdWbSunny className='sm-icon' /> : <BsFillMoonStarsFill className='sm-icon' />}
         <span className='sm-title'>Chủ đề</span>
+        <MdArrowForwardIos className='ar-icon'/>
       </div>
     </div>
   )
