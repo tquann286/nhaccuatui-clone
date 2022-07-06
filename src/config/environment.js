6238cdeeb2ad745bee40000ea95029beafc2a75d
Quan Trung <quantrung286@gmail.com>
2022-07-06 23:26:15 +0700

    basic firebase setup

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,3 +1,4 @@
 export const env = {
   DOMAIN_URL: process.env.REACT_APP_DOMAIN_URL,
+  FIREBASE_CONFIG: process.env.REACT_APP_FIREBASE_CONFIG,
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { env } from './environment'
+
+import {
+	getAuth,
+	GoogleAuthProvider,
+	FacebookAuthProvider,
+} from 'firebase/auth'
+import { getFirestore } from "firebase/firestore"
+
+
+const app = initializeApp(env.FIREBASE_CONFIG)
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
+const facebookProvider = new FacebookAuthProvider()
+
+const db = getFirestore(app)
+
+export { auth, googleProvider, facebookProvider, db }
