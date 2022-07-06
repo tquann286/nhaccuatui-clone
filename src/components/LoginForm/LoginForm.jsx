c4a590ddc34c7dd92ea978af8eb9309b52ede603
Quan Trung <quantrung286@gmail.com>
2022-07-06 17:24:04 +0700

    done header login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,14 +3,28 @@ import './LoginForm.scss'
 
 import { useStore, actions } from 'store'
 
+import { IoMdClose } from 'react-icons/io'
+
 const LoginForm = () => {
   const [state, dispatch] = useStore()
-  const { theme, lang, showLogin } = state
+  const { lang, showLogin } = state
+
+  
+
+  const toggleShowLogin = () => {
+    dispatch(actions.toggleShowLogin())
+  }
 
   return (
     <div className='lf-container' onClick={(e) => e.stopPropagation()}>
       <div className='lf-main'>
-        
+        <div className="lf-header">
+          <h4>{lang === 'vi' ? 'Đăng nhập' : 'Sign in'}</h4>
+          <button className='close-btn' onClick={toggleShowLogin}>
+            <IoMdClose />
+          </button>
+        </div>
+        <div className="lf-content"></div>
       </div>
     </div>
   )
