3fa25281cc32eedd023820775a818e6fe85bce74
Quan Trung <quantrung286@gmail.com>
2022-07-08 19:25:13 +0700

    auth form with formik

--- src/services/AuthForm.js
+++ src/services/AuthForm.js
@@ -1,19 +1,28 @@
-export const validateLogin = (values) => {
-  const errors = {}
-  
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (
-    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-  ) {
-    errors.email = 'Invalid email address';
-  }
-  return errors
+import * as Yup from 'yup'
+
+export const onSubmitLogin = (hihi) => {
+  console.log(hihi)
+  setTimeout(() => {
+    alert(JSON.stringify(hihi.values, null, 2));
+    hihi.setSubmitting(false);
+  }, 400);
 }
 
-export const onSubmitLogin = (values, { setSubmitting }) => {
-  setTimeout(() => {
-    alert(JSON.stringify(values, null, 2));
-    setSubmitting(false);
-  }, 400);
+export const validationAuthSchema = (handleAuthFunc) => {
+  const emailSchema = Yup.string().email(handleAuthFunc('Email không hợp lệ.', 'Invalid email.')).required(handleAuthFunc('Vui lòng điền vào trường này.', 'Email is required.')).trim(handleAuthFunc('Email không hợp lệ.', 'Invalid email'))
+  const passwordSchema = Yup.string().required().trim(handleAuthFunc('Mật khẩu không hợp lệ.', 'Invalid password'))
+
+  const loginSchema = Yup.object().shape({
+    email: emailSchema,
+    password: passwordSchema,
+  })
+
+  const signUpSchema = Yup.object().shape({
+    userName: Yup.string().required(handleAuthFunc('Vui lòng điền vào trường này.', 'Username is required.')).max(50, handleAuthFunc('Tên người dùng phải ít hơn 50 ký tự.', 'Username is must less than 50 characters.')),
+    email: emailSchema,
+    password: passwordSchema,
+    confirmPassword: Yup.string().required().trim(handleAuthFunc('Mật khẩu không hợp lệ.', 'Invalid password')).oneOf([Yup.ref('password'), null], handleAuthFunc('Mật khẩu nhập lại không đúng.', 'Incorrect re-enter password.'))
+  })
+
+  return handleAuthFunc(loginSchema, signUpSchema)
 }
