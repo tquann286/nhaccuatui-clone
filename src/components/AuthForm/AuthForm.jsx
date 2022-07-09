9ea9f099ab61eb62591e7a826250d943f6e30c3c
Quan Trung <quantrung286@gmail.com>
2022-07-09 21:32:40 +0700

    done layout auth form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AuthForm.scss'
 
 import { useForm } from 'react-hook-form'
@@ -19,6 +19,8 @@ const AuthForm = () => {
   const [state, dispatch] = useStore()
   const { lang, showLogin, showSignUp } = state
 
+  const [agreeTerm, setAgreeTerm] = useState(false)
+
   const defineLang = (vie, eng) => {
     return lang === 'vi' ? vie : eng
   }
@@ -29,7 +31,7 @@ const AuthForm = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: 'onBlur', reValidateMode: 'onBlur', resolver: yupResolver(authSchema) })
+  } = useForm({ mode: 'onBlur', reValidateMode: 'onBlur' })
 
   const toggleShowLogin = () => {
     dispatch(actions.toggleShowLogin())
@@ -63,24 +65,26 @@ const AuthForm = () => {
         <div className='af-content'>
           <div className='form-container'>
             <form onSubmit={handleSubmit(handleAuthFunc(onLoginSubmit, onSignUpSubmit))}>
-              <div className='input-container username'>
-                <div className='input-main'>
-                  <AiOutlineUser className='input-icon' />
-                  <input type='text' {...register('username')} placeholder={defineLang('Tên hiển thị', 'Username')} onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e)} />
-                  {showSignUp && (
-                    <div className='more-info username'>
-                      <BsInfoCircle className='more-info-icon' />
-                      <div className='more-info-description'>
-                        <p>{defineLang('Bạn có thể sử dụng chữ cái, chữ số, gạch dưới và dấu chấm. Chiều dài tối đa 30 kí tự', 'You can use letters, numbers, underscores and dots. Length from 6-30 characters')}</p>
+              {showSignUp && (
+                <div className='input-container username'>
+                  <div className='input-main'>
+                    <AiOutlineUser className='input-icon' />
+                    <input type='text' {...register('username')} placeholder={defineLang('Tên hiển thị', 'Username')} onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e)} />
+                    {showSignUp && (
+                      <div className='more-info username'>
+                        <BsInfoCircle className='more-info-icon' />
+                        <div className='more-info-description'>
+                          <p>{defineLang('Bạn có thể sử dụng chữ cái, chữ số, gạch dưới và dấu chấm. Chiều dài tối đa 30 kí tự', 'You can use letters, numbers, underscores and dots. Length from 6-30 characters')}</p>
+                        </div>
                       </div>
-                    </div>
-                  )}
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="input-container email">
-                <div className="input-main">
+              )}
+              <div className='input-container email'>
+                <div className='input-main'>
                   <HiOutlineMail className='input-icon' />
-                  <input type="email" {...register('email')} placeholder='Email' onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e)} />
+                  <input type='email' {...register('email')} placeholder='Email' onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e)} />
                   {showSignUp && (
                     <div className='more-info email'>
                       <BsInfoCircle className='more-info-icon' />
@@ -91,6 +95,50 @@ const AuthForm = () => {
                   )}
                 </div>
               </div>
+              <div className='input-container password'>
+                <div className='input-main'>
+                  <BsKeyboard className='input-icon' />
+                  <input type='password' {...register('password')} placeholder={defineLang('Mật khẩu', 'Password')} onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e)} />
+                  {showSignUp && (
+                    <div className='more-info password'>
+                      <BsInfoCircle className='more-info-icon' />
+                      <div className='more-info-description'>
+                        <p>{defineLang('Chiều dài từ 6-30 kí tự. Không sử dụng tiếng Việt có dấu', 'Length from 6-30 characters. Do not use accented Vietnamese')}</p>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+              {showSignUp && (
+                <div className='input-container comfirmedPassword'>
+                  <div className='input-main'>
+                    <BsKeyboard className='input-icon' />
+                    <input type='password' {...register('comfirmedPassword')} placeholder={defineLang('Nhập lại mật khẩu', 'Re-enter Password')} onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e)} />
+                    {showSignUp && (
+                      <div className='more-info comfirmedPassword'>
+                        <BsInfoCircle className='more-info-icon' />
+                        <div className='more-info-description'>
+                          <p>{defineLang('Nhập lại mật khẩu giống như bên trên một lần nữa', 'Re-enter the same password as above again')}</p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
+              {showSignUp && (
+                <div className='term-container'>
+                  <input id='term-confirm' type='checkbox' className='term-checkbox' checked={agreeTerm} onChange={() => setAgreeTerm(!agreeTerm)} />
+                  <label htmlFor='term-confirm' className='term-content'>
+                    {defineLang('Tôi đã đọc và đồng ý với các ', 'I have read and agree to the ')}
+                  </label>
+                  <a href={TERM_LINK} className='link-term' target='_blank' rel='noopener'>
+                    {defineLang('Điều khoản', 'Terms')}
+                  </a>
+                </div>
+              )}
+              <button type='submit' className={`submit-btn ${showSignUp && 'sign-up'} ${agreeTerm || 'disabled'}`} disabled={!agreeTerm}>
+                {handleAuthFunc(defineLang('Đăng nhập', 'Sign in'), defineLang('Đăng ký', 'Sign up'))}
+              </button>
             </form>
           </div>
         </div>
