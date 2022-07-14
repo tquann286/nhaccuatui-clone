4ff8a58e5d48ed85885e1369b9644077c64d1103
Quan Trung <quantrung286@gmail.com>
2022-07-14 11:37:36 +0700

    option modal

---
+++ src/components/Common/OptionModal/OptionModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createPortal } from 'react-dom'
+
+import './OptionModal.scss'
+
+const OptionModal = ({ showModal, positionRef, selfRef }) => {
+
+
+  return createPortal(
+    <div className='om-container'>
+      
+    </div>
+  )
+}
+
+export default OptionModal
