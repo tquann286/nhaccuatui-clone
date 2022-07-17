8acc4978dfe0a1cc248600de969fc7ef1a925366
Quan Trung <quantrung286@gmail.com>
2022-07-17 14:58:21 +0700

    fix error if page is loading

--- src/hooks/useGetPosition.js
+++ src/hooks/useGetPosition.js
@@ -3,9 +3,9 @@ import { useLayoutEffect } from 'react'
 const useGetPosition = (ref, handle, showModal) => {
   useLayoutEffect(() => {
     if (ref) {
-			const { right } = ref.current.getBoundingClientRect()
-			const top = ref.current.offsetTop || ref.current.getBoundingClientRect().y + window.pageYOffset
-      handle(right, top)
+			const position = ref.current?.getBoundingClientRect()
+			const top = ref.current?.offsetTop || ref.current?.getBoundingClientRect().y + window.pageYOffset
+      handle(position?.right, top)
     }
   }, [showModal, ref])
 }
