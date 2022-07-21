282a2d67316ad1c3b7334146a44287cb6db15d2b
Quan Trung <quantrung286@gmail.com>
2022-07-21 11:11:08 +0700

    fix intermediate value when destructuring

--- src/hooks/useGetFixedPosition.js
+++ src/hooks/useGetFixedPosition.js
@@ -3,9 +3,9 @@ import { useLayoutEffect } from 'react'
 const useGetFixedPosition = (ref, handle, showModal) => {
   useLayoutEffect(() => {
     if (ref) {
-			const { right } = ref.current?.getBoundingClientRect()
+			const position = ref.current?.getBoundingClientRect()
 			const top = ref.current?.offsetTop
-      handle(right, top)
+      handle(position?.right, top)
     }
   }, [showModal, ref])
 }
--- src/hooks/useGetPosition.js
+++ src/hooks/useGetPosition.js
@@ -3,9 +3,9 @@ import { useLayoutEffect } from 'react'
 const useGetPosition = (ref, handle, showModal) => {
   useLayoutEffect(() => {
     if (ref) {
-			const { right } = ref.current?.getBoundingClientRect()
+			const position = ref.current?.getBoundingClientRect()
 			const top =ref.current?.getBoundingClientRect().y + window.pageYOffset ||  ref.current?.offsetTop
-      handle(right, top)
+      handle(position?.right, top)
     }
   }, [showModal, ref])
 }
