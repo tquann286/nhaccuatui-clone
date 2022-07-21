46e23091da1bb6ec76b667351a6a07cedaf4d791
Quan Trung <quantrung286@gmail.com>
2022-07-21 19:31:18 +0700

    done all page search result

--- src/components/Common/CommonVideo/CommonVideo.jsx
+++ src/components/Common/CommonVideo/CommonVideo.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import './CommonVideo.scss'
 
-import { VideoOverlay } from 'components'
-import { createVideoUrl, handleCopyVideo } from 'share/utilities'
+import { CommonArtist, VideoOverlay } from 'components'
+import { createVideoUrl } from 'share/utilities'
 
 import { useStore } from 'store'
 
@@ -33,6 +33,10 @@ const CommonVideo = ({ keyId, artists, duration, refMapping, thumbnail, title, v
       <div className='cv-video border-0-05' style={{ height: videoHeight }}>
         <VideoOverlay {...videoOverlayProps} />
       </div>
+      <div className="cv-title color-0-88 small-title" title={title}>
+        <Link to={createVideoUrl(keyId, title, artists)}>{title}</Link>
+      </div>
+      <CommonArtist artists={artists} />
     </div>
   )
 }
