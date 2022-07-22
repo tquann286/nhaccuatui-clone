918fa401fb265a9a49b3105bd2393c95964124e2
Quan Trung <quantrung286@gmail.com>
2022-07-22 23:57:54 +0700

    song result header

--- src/components/Search/TopResult/SongResult/SongResult.jsx
+++ src/components/Search/TopResult/SongResult/SongResult.jsx
@@ -3,10 +3,13 @@ import './SongResult.scss'
 
 import { SongSquare } from 'components'
 
-const SongResult = ({ defineLang }) => {
+const SongResult = ({ defineLang, total }) => {
   return (
     <div className='song-result-container common-section'>
-      
+      <div className='song-result-title color-0-88 search-header'>
+        {defineLang('Bài hát ', 'Song ')}
+        <span className='color-0-5'>{defineLang(`(Có ${total} kết quả)`, `${total > 1 ? `(There are ${total} results)` : `(There is ${total} result)`}`)}</span>
+      </div>
     </div>
   )
 }
