4ea8f6d7c4b32bcd7838fa006950b9e95df366ff
Quan Trung <quantrung286@gmail.com>
2022-07-23 14:05:21 +0700

    add init image to fav song

--- src/components/User/FavoriteMain/SongFav/SongFav.jsx
+++ src/components/User/FavoriteMain/SongFav/SongFav.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './SongFav.scss'
 
+import initImage from 'images/default/default_personal_playlist.png'
+import { ShadowThumb } from 'components'
+
 const SongFav = () => {
   return (
     <div className='song-fav-container'>
       <div className="sf-header w3-row">
-
+        <div className="sf-thumb-img w3-col">
+          <ShadowThumb width='16rem' shadowHeight='0.6rem' imageUrl={initImage}  />
+        </div>
       </div>
     </div>
   )
