080cc3f76f8f0f6dbf7607aea491d797dd6e2e18
Quan Trung <quantrung286@gmail.com>
2022-07-24 20:33:26 +0700

    more option to song item

--- src/components/User/FavoriteMain/SongFav/SongItem.jsx
+++ src/components/User/FavoriteMain/SongFav/SongItem.jsx
@@ -1,15 +1,24 @@
+import { IconButton } from '@mui/material'
 import { CommonArtist } from 'components'
 import { BsHeadphones } from 'react-icons/bs'
+import { IoMdMore } from 'react-icons/io'
 import { formatNumber } from 'share'
 
 const SongItem = ({ keyId, songId, key, title, artists, duration, songsView }) => {
   return (
-    <li key={keyId || songId || key} className='song-list-common bg-color-0-02 li-list-item-common color-0-6 hover-bg-color-0-05'>
+    <li key={keyId || songId || key} className='song-list-common bg-color-0-02 li-list-item-common color-0-6 hover-bg-color-0-05 hover-visible'>
       <div className='song-list-title-artist'>
         <div className='song-list-title song-list-title-real'>
           <div className='alcenter-jcbetween'>
             <div className='alcenter' style={{ overflow: 'hidden' }}>
               <div className="song-list-title-scss color-0-88" title={title}>{title}</div>
+            </div>
+            <div className="alcenter fit-width">
+              <div className="vi-hidden">
+                <IconButton className='more-btn' aria-label="more">
+                  <IoMdMore />
+                </IconButton>
+              </div>
             </div>
           </div>
         </div>
