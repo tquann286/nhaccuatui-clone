85f7bf32035cfb31ecc61a70b60ebec03bcc36c4
Quan Trung <quantrung286@gmail.com>
2022-07-26 22:54:16 +0700

    done new hot page

--- src/components/SongMain/NewHot/NewHot.jsx
+++ src/components/SongMain/NewHot/NewHot.jsx
@@ -8,7 +8,6 @@ import { calcPaginationPage } from 'share/utilities'
 
 const NewHot = ({ defineLang }) => {
   const [newHot, setNewHot] = useState(null)
-  console.log('newHot: ', newHot)
   const [pageIndex, setPageIndex] = useState(1)
   const [isLoading, setIsLoading] = useState(false)
 
@@ -31,11 +30,34 @@ const NewHot = ({ defineLang }) => {
       </div>
     )
 
+  if (!newHot) return null
+
+  const { data: songs, total } = newHot
+
+  const pagiProps = {
+    pageIndex,
+    setPageIndex,
+    count: calcPaginationPage(total),
+    defineLang,
+  }
+
   return (
     <div className='new-hot-container common-section common-marginTLR'>
-      <div className='new-hot-title color-0-88 common-title'>
-        {defineLang('Mới & Hot', 'New & Hot')}
+      <div className='new-hot-title color-0-88 common-title padding-bottom-1-2'>{defineLang('Mới & Hot', 'New & Hot')}</div>
+      <div className='new-hot-main'>
+        <Grid container spacing={2}>
+          {songs.map((song) => (
+            <Grid item key={song.key} xs={3} sm={3} md={3} xl={2}>
+              <SongSquare {...song} keyId={song.key} />
+            </Grid>
+          ))}
+        </Grid>
       </div>
+      {calcPaginationPage(total) > 1 && (
+        <div className='common-marginTLR'>
+          <PagiCommon {...pagiProps} />
+        </div>
+      )}
     </div>
   )
 }
