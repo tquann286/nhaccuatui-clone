92401405b82c14434ba2ee82539245a793daabdc
Quan Trung <quantrung286@gmail.com>
2022-08-23 09:13:18 +0700

    add footer to playlist detail

--- src/components/Playlist/PlaylistDetail.jsx
+++ src/components/Playlist/PlaylistDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import parse from 'html-react-parser'
 
 import { useStore } from 'store'
-import { CircleTitleArtist, SongList, ListTag, LoadingV2, Provider, ShadowThumb, Sharing, Title, TitleCommon, MaybeLike } from 'components'
+import { CircleTitleArtist, SongList, ListTag, LoadingV2, Provider, ShadowThumb, Sharing, Title, TitleCommon, MaybeLike, Footer } from 'components'
 import { getCurrentPathname, getListSongsKey, getMaybeLike, getSongsView, handleCopyProxy } from 'share/utilities'
 import { getPlaylistDetailData } from 'services/Playlist/Playlist'
 import { IconButton, Tooltip } from '@mui/material'
@@ -78,36 +78,39 @@ const PlaylistDetail = () => {
   }
 
   return (
-    <div className='commonMainOutlet pt-24px px-32px relative'>
-      {artists.length !== 0 && <Title title={`${title} - ${artists.map((art) => art.name).join(', ')} - NhacCuaTui Clone`} />}
-      <div className='w3-row'>
-        <div className='w3-col w-240px'>
-          <ShadowThumb imageUrl={thumbnail} width='24rem' />
-        </div>
-        <div className='w3-rest pl-24px'>
-          <TitleCommon type='playlist' defineLang={defineLang} title={title} />
-          <CircleTitleArtist circleStyles='float-left' titleStyles='!mt-unset ml-8px' artists={artists} />
-          <div className='w3-row mt-4'>
-            <div className='w3-rest text-13px color-0-5 w-fit'>{new Date(dateCreate).toLocaleDateString()}</div>
+    <div className='commonMainOutlet'>
+      <div className='pt-24px px-32px relative margin-footer'>
+        {artists.length !== 0 && <Title title={`${title} - ${artists.map((art) => art.name).join(', ')} - NhacCuaTui Clone`} />}
+        <div className='w3-row'>
+          <div className='w3-col w-240px'>
+            <ShadowThumb imageUrl={thumbnail} width='24rem' />
           </div>
-          <div className='mt-8px text-sm color-0-5'>{parse(description)}</div>
-          <ListTag listTag={listTag} defineLang={defineLang} />
+          <div className='w3-rest pl-24px'>
+            <TitleCommon type='playlist' defineLang={defineLang} title={title} />
+            <CircleTitleArtist circleStyles='float-left' titleStyles='!mt-unset ml-8px' artists={artists} />
+            <div className='w3-row mt-4'>
+              <div className='w3-rest text-13px color-0-5 w-fit'>{new Date(dateCreate).toLocaleDateString()}</div>
+            </div>
+            <div className='mt-8px text-sm color-0-5'>{parse(description)}</div>
+            <ListTag listTag={listTag} defineLang={defineLang} />
+          </div>
         </div>
-      </div>
-      <div className='w-full h-64px rounded-4px bg-color-0-02 mt-24px px-24px py-12px flex justify-between'>
-        <Provider { ... providerProps } />
-        <div className='flex items-center'>
-          <Tooltip title={defineLang('Thêm vào yêu thích', 'Add to favorite')} placement='top' arrow enterDelay={400}>
-            <IconButton size='large' onClick={() => handleAddToFavPlaylist(key, defineLang)}>
-              <BsBookmarkPlus />
-            </IconButton>
-          </Tooltip>
-          <Sharing {...sharingProps} />
+        <div className='w-full h-64px rounded-4px bg-color-0-02 mt-24px px-24px py-12px flex justify-between'>
+          <Provider {...providerProps} />
+          <div className='flex items-center'>
+            <Tooltip title={defineLang('Thêm vào yêu thích', 'Add to favorite')} placement='top' arrow enterDelay={400}>
+              <IconButton size='large' onClick={() => handleAddToFavPlaylist(key, defineLang)}>
+                <BsBookmarkPlus />
+              </IconButton>
+            </Tooltip>
+            <Sharing {...sharingProps} />
+          </div>
         </div>
+        <div className='mt-44px mb-16px text-22px font-bold color-0-88'>{defineLang('Danh sách bài hát', 'Song list')}</div>
+        <SongList listSong={songs} defineLang={defineLang} addToFav songsView={songsView} />
+        <MaybeLike defineLang={defineLang} maybeLike={maybeLike} />
       </div>
-      <div className="mt-44px mb-16px text-22px font-bold color-0-88">{defineLang('Danh sách bài hát', 'Song list')}</div>
-      <SongList listSong={songs} defineLang={defineLang} addToFav songsView={songsView} />
-      <MaybeLike defineLang={defineLang} maybeLike={maybeLike} />
+      <Footer />
     </div>
   )
 }
