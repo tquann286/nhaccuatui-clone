f45473c63e4fbfe63b885d21f82afb1718834f5a
Quan Trung <quantrung286@gmail.com>
2022-08-26 07:52:43 +0700

    use image for user avatar

--- src/components/User/UpdateUser.jsx
+++ src/components/User/UpdateUser.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
+import { Image } from 'components'
+import no_user_img from 'images/default/default_user.jpg'
+
 const UpdateUser = ({ defineLang, photoURL = '' }) => {
-  // 
   return (
     <div>
       <div className='mt-28px text-22px font-bold color-0-88'>{defineLang('Cập Nhật Thông Tin', 'Update Profile')}</div>
       <div className='mt-24px'>
-        <div className='flex items-center mb-8px'>
+        <div className='update-user-field'>
           <p className='text-13px color-0-5 w-40 font-medium'>{defineLang('Hình đại diện', 'Avatar')}:</p>
-          <div className={`w-64px h-64px rounded-4px mr-17px bg-cover bg-no-repeat`} style={{ backgroundImage: `url(${photoURL})` }}></div>
+          <Image className='w-64px h-64px rounded-4px mr-17px' imageUrl={photoURL} backupImg={no_user_img} />
           <div>
             <input type='file' id='user-thumb' name='user-thumbnail' accept='image/*' className='hidden' />
             <label htmlFor='user-thumb'>
