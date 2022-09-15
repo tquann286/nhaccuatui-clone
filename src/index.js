2e55fa139bb5ef73b2fa643c2dbb4e54bae41eff
Quan Trung <quantrung286@gmail.com>
2022-09-15 17:58:25 +0700

    update index

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { HelmetProvider } from 'react-helmet-async'
 import { StyledEngineProvider } from '@mui/material'
 import { StoreProvider } from 'store'
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
